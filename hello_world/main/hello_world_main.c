ff6b7bf5809cb12994db9cf3bb7161170dd214b1
Brian Bulkowski <brian@bulkowski.org>
2020-01-03 11:40:41 -0800

    A simple hello world that works.

--- hello_world/main/hello_world_main.c
+++ hello_world/main/hello_world_main.c
@@ -9,13 +9,14 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
 
 void app_main()
 {
-    printf("Hello world!\n");
+    printf("Hello world! VERSION 2!\n");
 
     /* Print chip information */
     esp_chip_info_t chip_info;
@@ -30,7 +31,7 @@ void app_main()
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 10; i >= 0; i--) {
+    for (int i = 20; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
